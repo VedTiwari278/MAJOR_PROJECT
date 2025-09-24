9617f0570b135566e26c90b444f40f073039f206
VedTiwari278 <148948448+VedTiwari278@users.noreply.github.com>
2025-09-24 19:23:12 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./app.css";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import NavBar from "./Component/NavBar";
